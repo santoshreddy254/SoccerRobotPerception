f8691e7ea2416ac2f0b5257d0a565086ab46fd43
santoshreddy254 <santoshreddy45@yahoo.com>
2021-03-05 19:00:33 +0100

    chnaged axis in variational loss

--- losses.py
+++ losses.py
@@ -6,13 +6,13 @@ detection_loss = nn.MSELoss()
 segmentation_loss = nn.CrossEntropyLoss()
 
 def total_variation(images, name=None):
-    "Adapted from tf.image.total_variation" 
+    "Adapted from tf.image.total_variation"
     "(https://www.tensorflow.org/api_docs/python/tf/image/total_variation) "
-    
+
     pixel_dif1 = images[1:, :, :] - images[:-1, :, :]
     pixel_dif2 = images[:, 1:, :] - images[:, :-1, :]
 
     # Sum for all axis. (None is an alias for all axis.)
-    sum_axis = None
-    tot_var = (torch.sum(torch.abs(pixel_dif1), axis=sum_axis) + torch.sum(torch.abs(pixel_dif2), axis=sum_axis))
+    # sum_axis = None
+    tot_var = (torch.sum(torch.abs(pixel_dif1)) + torch.sum(torch.abs(pixel_dif2)))
     return tot_var
